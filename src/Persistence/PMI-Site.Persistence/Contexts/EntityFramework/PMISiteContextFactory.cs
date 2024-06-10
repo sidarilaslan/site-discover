cc85e8ba627350bedebad653f1ff2519e3119270
sidar <ilaslansidar@gmail.com>
2024-06-10 22:52:28 +0300

    add design time context factory

---
+++ src/Persistence/PMI-Site.Persistence/Contexts/EntityFramework/PMISiteContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMI_Site.Persistence.Contexts.EntityFramework
+{
+    public class PMISiteContextFactory : IDesignTimeDbContextFactory<PMISiteContext>
+    {
+        public PMISiteContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<PMISiteContext>()
+                .UseSqlServer(connectionString);
+
+            return new PMISiteContext(optionsBuilder.Options);
+        }
+    }
+}
