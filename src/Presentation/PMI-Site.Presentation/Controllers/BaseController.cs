e906e74ae9913dac66899a5d0cd1d003d563c610
sidar <ilaslansidar@gmail.com>
2024-06-12 01:30:41 +0300

    add baseController

---
+++ src/Presentation/PMI-Site.Presentation/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PMI_Site.Presentation.Controllers
+{
+    public class BaseController : Controller
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
