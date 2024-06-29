cc3aa9c0b8b664a35cb7aed4c287268f72730a73
sidar <ilaslansidar@gmail.com>
2024-06-30 00:54:38 +0300

    add health check middleware

--- src/Presentation/PMI-Site.Presentation/Program.cs
+++ src/Presentation/PMI-Site.Presentation/Program.cs
@@ -1,5 +1,9 @@
 using PMI_Site.Persistence;
 using PMI_Site.Application;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Net.Mime;
+using Newtonsoft.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +12,7 @@ builder.Services.AddControllersWithViews();
 builder.Configuration.AddJsonFile($"appsettings.{envName}.json", optional: false, reloadOnChange: true);
 builder.Services.AddPersistanceService(builder.Configuration);
 builder.Services.AddApplicationService();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -24,6 +29,20 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseHealthChecks("/app/health", new HealthCheckOptions
+{
+    ResponseWriter = async (context, healthReport) =>
+    {
+        string result = JsonConvert.SerializeObject(new
+        {
+            status = healthReport.Status.ToString(),
+          
+        });
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
