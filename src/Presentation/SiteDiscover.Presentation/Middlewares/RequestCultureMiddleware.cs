d9d58ed4e6318779bb6eaa5c174b56675290e013
sidar <ilaslansidar@gmail.com>
2024-07-13 01:34:40 +0300

    add RequestCultureMiddleware

---
+++ src/Presentation/SiteDiscover.Presentation/Middlewares/RequestCultureMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Localization;
+
+namespace SiteDiscover.Presentation.Middlewares
+{
+    public class RequestCultureMiddleware
+    {
+        readonly RequestDelegate _next;
+      
+
+        public RequestCultureMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            IRequestCultureFeature? requestCultureFeature = context.Features.Get<IRequestCultureFeature>();
+            string? uiCulture = requestCultureFeature.RequestCulture.UICulture.ToString();
+
+            if (context.Request.Path == "/")
+                context.Response.Redirect($"/{uiCulture}");
+            
+            await _next(context);
+           
+        }
+    }
+}
